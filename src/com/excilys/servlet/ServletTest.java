32214c9279a417f72fcc3d138e0d753473ac0834
dlisbona <dlisbona@excilys.com>
2019-09-24 11:47:28 +0200

    Connexion JSP test OK

--- src/com/excilys/servlet/ServletTest.java
+++ src/com/excilys/servlet/ServletTest.java
@@ -1,30 +1,17 @@
 package com.excilys.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-
+@SuppressWarnings("serial")
 public class ServletTest extends HttpServlet {
-
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    response.setContentType("text/html");
-    response.setCharacterEncoding("UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta charset=\"utf-8\" />");
-    out.println("<title>Test</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<p>Ceci est une page générée depuis une servlet.</p>");
-    out.println("</body>");
-    out.println("</html>");
+    request.getRequestDispatcher("/WEB-INF/test.jsp").forward(request, response);
   }
 }
+
